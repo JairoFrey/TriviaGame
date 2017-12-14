0dd70f1e8e8fea8f2c9e595e81e1d1ecb965f1b7
JairoFrey <jayro15@yahoo.com>
2017-12-14 08:45:08 -0600

    Add files via upload

--- TriviaGame/assets/javascript/app.js
+++ TriviaGame/assets/javascript/app.js
@@ -1,18 +1,22 @@
 // set variables
 var timer = 60;
+var questions = 0;
+var correct = 0;
 var option1 = [];
 var option2 = [];
 var option3 = [];
 var option4 = [];
+var option5 = []
 var pictures = ["assets/images/beth-NY.jpg", "assets/images/kelly-OC.jpg", "assets/images/kerrie-D.jpg", "assets/images/kyle-BH.jpg", "assets/images/teresa-NJ.jpg"];
 // var options = ["Real Housewives of Orange County", "Real Housewives of New Jersey", "Real Housewives of Beverly Hills", "Real Housewives of Dallas", "Real Housewives of New York"];
-var correct = 0;
-var incorrect = 0;
 
-alert("poop");
+
+
+
+
+function game() {
+
 // function to choose random picture for first question. Must use math.random for it to choose random picture in array.
-console.log(correct);
-
 function randomWife() {
 var randomPic = pictures[Math.floor(Math.random()*pictures.length)];
 var pic = $("<img>");
@@ -22,85 +26,112 @@ $("#picture").append(pic);
 }
 
 randomWife();
+console.log(randomWife);
 
 
-
-// function that goes to next question and picture when question is answered or timer gets to 0
+// function that goes to next picture when question is answered and adds to question variable
 function nextQuestion(){
 	randomWife();
+	questions++;
+	if (questions >= 5) {
+	end();
+	location.reload();
 }
+};
 
-// function to have timer countdown from 10 when new picture is chosen
+// functionon that gives alert once you've went through 5 pictures
+function end () {
+	if (correct >= 4 && questions == 5) {
+		alert("Congratulations!! You scored " + correct +" out of 5. Maybe get a new hobby or read a book.");
+	}
+	else if (correct <= 3 && questions == 5) {
+		alert("Congratulations!! You scored " + correct + " out of 5.");
+	}
+};
 
+// function to have timer countdown from 60 when page is loaded
 
 var countdown = setInterval(function(){
 	timer--;
 	$("#time").text(timer);
 	if (timer == 0) {
-		nextQuestion();
+		end();
 		clearInterval(countdown);
 	}}, 1000);
 
 // create on click functions which match a given picture to the button on screen
+
+var randomPic = pictures[Math.floor(Math.random()*pictures.length)];
+
+var pic = String(randomPic);
+
 $("#option1").on("click", function(){
-	if (randomPic == pictures[2]) {
+	if (pic == pictures[2]) {
 		correct++;
+		console.log(correct);
+		$("#picture").empty();
 		nextQuestion();
 	}
-	else if () {
-		incorrect++;
+	 else {
+		$("#picture").empty();
 		nextQuestion();
 	};
 });
 
 $("#option2").on("click", function() {
-	if (randomPic == pictures[0]) {
+	if (pic == pictures[0]) {
 		correct++;
+		console.log(correct);
+		$("#picture").empty();
 		nextQuestion();
 	}
-	else () {
-		incorrect++;
+	else {
+		$("#picture").empty();
 		nextQuestion();
 	}
 });
 
 $("#option3").on("click", function() {
-	if (randomPic == pictures[4]) {
+	if (pic == pictures[4]) {
 		correct++;
+		console.log(correct);
+		$("#picture").empty();
 		nextQuestion();
 	}
-	else () {
-		incorrect++;
+	else {
+		$("#picture").empty();
 		nextQuestion();
 	}
 });
 
 $("#option4").on("click", function() {
-	if (randomPic == pictures[1]) {
+	if (pic == pictures[1]) {
 		correct++;
+		console.log(correct);
+		$("#picture").empty();
 		nextQuestion();
 	}
-	else () {
-		incorrect++;
+	else {
+		$("#picture").empty();
 		nextQuestion();
 	}
 });
-$("option5").on("click", function() {
-	if (randomPic == pictures[3]) {
+$("#option5").on("click", function() {
+	if (pic == pictures[3]) {
 		correct++;
+		console.log(correct);
+		$("#picture").empty();
 		nextQuestion();
 	}
-	else () {
-		incorrect++;
+	else {
+		$("#picture").empty();
 		nextQuestion();
 	}
+	
 });
 
+};
 
-// function that makes sure no duplicate answer options are displayed
+game();
 
-
-
-// function that keeps score and will alert you of your score and smart ass response after 5 pictures have been reviewed
-
-// function that resets score aftere alert is shown
+// the correct variable only increases by one on one certain picture when the page is reloaded. Not sure why but I'm working on it.
